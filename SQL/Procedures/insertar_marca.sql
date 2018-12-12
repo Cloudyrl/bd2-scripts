2fc416ef707396f001a137017a0469edd1aca1b2
pedroale <pedro.alejandro.pt@gmail.com>
2018-12-11 23:22:46 -0400

    Procedimiento Insertar Marca Casi Listo, Direcciones para llamarlo desde SQL PLUS y SQL Developer. Cambios en produccion y exportacion de los inserts & Funcion Conversion Volumen lista

--- SQL/Procedures/insertar_marca.sql
+++ SQL/Procedures/insertar_marca.sql
@@ -1,33 +1,48 @@
-Create or Replace Procedure insert_marca
-begin
-declare
-    v_nombre Varchar2(50);
-    v_elaboracion Varchar2(1000);
-    v_cata_propia Varchar2(1000);
-    v_maridaje Varchar2(1000);
-    v_temperatura Number(10);
-    v_unidad_temperatura Varchar2(5);
-    v_ventana_de_consumo Number(10);
-    v_grado_de_alcohol Float;
-    v_acidez_total Float;
-    v_ph Float;
-    v_color Varchar2(50);
-    begin
- 
-        v_nombre := '&nombre';
-        v_elaboracion := '&elaboracion';
-        v_cata_propia := '&cata_propia';
-        v_maridaje := '&maridaje';
-        v_temperatura := '&temperatura';
-        v_unidad_temperatura := '&unidad_de_temperatura';
-        v_ventana_de_consumo := '&ventana_de_consumo';
-        v_grado_de_alcohol := '&grado_de_alcohol';
-        v_acidez_total := '&acidez_total';
-        v_ph := '&ph';
-        v_color := '&color';
+Create or Replace PROCEDURE pr_insertar_marca
+    (v_nombre in Varchar2,
+    v_elaboracion in Varchar2,
+    v_cata_propia in Varchar2,
+    v_maridaje in Varchar2,
+    v_temperatura in Number,
+    v_unidad_temperatura in Varchar2,
+    v_ventana_de_consumo in Number,
+    v_grado_de_alcohol in Varchar2,
+    v_acidez_total in Varchar2,
+    v_ph in Varchar2,
+    v_color in Varchar2,
+    v_contacto_madera in Varchar2,
+    v_tipo_tapon in Varchar2,
+    v_tiempo_maduracion in Number,
+    v_bodega in Varchar2,
+    v_clasificacion in Varchar2)
+IS
+    v_clave_bodega Number;
+    v_clave_clasificacion Number;
+BEGIN
     
-            insert into marca values ((select count(*)+1 from marca), v_nombre, v_elaboracion, v_cata_propia, v_maridaje , v_temperatura, v_unidad_temperatura, v_ventana_de_consumo, v_grado_de_alcohol, v_acidez_total, v_ph, v_color, empty_blob(),'Y','Corcho',Calificacion_marca_nt(Calificacion('James Halliday', Tipo_valor('01-01-2017', 95)), Calificacion('Huon Hooke',Tipo_valor('01-01-2017', 92)), Calificacion('Mike Bennie',Tipo_valor('01-01-2016', 93))),Tipo_valor_marca_nt(Tipo_valor('01-01-2015',600),Tipo_valor('01-01-2016',900),Tipo_valor('01-01-2017',950)),Distribucion_exp_marca_nt(Distribucion_exp(Tipo_valor('01-01-2015', 100),'Uruguay'),Distribucion_exp(Tipo_valor('01-01-2016', 100), 'Uruguay'),Distribucion_exp(Tipo_valor('01-01-2017', 100),'Uruguay'),Distribucion_exp(Tipo_valor('01-01-2015', 50),'Japon'),Distribucion_exp(Tipo_valor('01-01-2016', 150),'Japon'),Distribucion_exp(Tipo_valor('01-01-2017', 150),'Japon'),Distribucion_exp(Tipo_valor('01-01-2015', 190),'Hungria'),Distribucion_exp(Tipo_valor('01-01-2016', 100),'Hungria'),Distribucion_exp(Tipo_valor('01-01-2017', 100),'Hungria')),15,4,2);
-    
-    end;
-end insert_marca;
-/
+    Select b.clave  into v_clave_bodega From Bodega b Where b.nombre = v_bodega;
+    Select c.clave into v_clave_clasificacion From Clasificacion c Where c.nombre = v_clasificacion;
+    Insert into marca values (
+        (select count(*)+1 from marca), 
+        v_nombre, 
+        v_elaboracion,
+        v_cata_propia, 
+        v_maridaje, 
+        v_temperatura, 
+        v_unidad_temperatura, 
+        v_ventana_de_consumo, 
+        TO_NUMBER(v_grado_de_alcohol), 
+        TO_NUMBER(v_acidez_total), 
+        TO_NUMBER(v_ph), 
+        v_color, 
+        empty_blob(),
+        v_contacto_madera,
+        v_tipo_tapon,
+        Calificacion_marca_nt(Calificacion('James Halliday', Tipo_valor('01-01-2017', 95)), Calificacion('Huon Hooke',Tipo_valor('01-01-2017', 92)), Calificacion('Mike Bennie',Tipo_valor('01-01-2016', 93))),
+        Tipo_valor_marca_nt(Tipo_valor('01-01-2015',600),Tipo_valor('01-01-2016',900),Tipo_valor('01-01-2017',950)),
+        Distribucion_exp_marca_nt(Distribucion_exp(Tipo_valor('01-01-2015', 100),'Uruguay'),Distribucion_exp(Tipo_valor('01-01-2016', 100), 'Uruguay'),Distribucion_exp(Tipo_valor('01-01-2017', 100),'Uruguay'),Distribucion_exp(Tipo_valor('01-01-2015', 50),'Japon'),Distribucion_exp(Tipo_valor('01-01-2016', 150),'Japon'),Distribucion_exp(Tipo_valor('01-01-2017', 150),'Japon'),Distribucion_exp(Tipo_valor('01-01-2015', 190),'Hungria'),Distribucion_exp(Tipo_valor('01-01-2016', 100),'Hungria'),Distribucion_exp(Tipo_valor('01-01-2017', 100),'Hungria')),
+        v_tiempo_maduracion,
+        v_clave_bodega,
+        v_clave_clasificacion);
+
+END pr_insertar_marca;
---
+++ SQL/Procedures/insertar_marca_exec.sql
@@ -0,0 +1,22 @@
+    
+    variable v_grado_de_alcohol Varchar2(10);
+    variable v_acidez_total Varchar2(10);
+    variable v_ph Varchar2(10);
+    accept v_nombre char prompt "Ingrese el nombre del vino: ";
+    accept v_elaboracion char prompt "Ingrese la elaboracion: ";
+    accept v_cata_propia char prompt "Ingrese la cata propia: ";
+    accept v_maridaje char prompt "Ingrese el maridaje: ";
+    accept v_temperatura prompt "Ingrese la temperatura: ";
+    accept v_unidad_temperatura char prompt "Ingrese la unidad de temperatura: ";
+    accept v_ventana_de_consumo prompt "Ingrese la ventana de consumo: ";
+    accept v_grado_de_alcohol prompt "Ingrese el grado de alcohol: ";
+    accept v_acidez_total prompt "Ingrese la acidez total: ";
+    accept v_ph prompt "Ingrese el ph: ";
+    accept v_color char prompt "Ingresar el color: ";
+    accept v_contacto_madera char prompt "Ingresar si tiene contacto con madera (Y,N): ";
+    accept v_tipo_tapon char prompt "Ingresar el tipo de tapon: ";
+    accept v_tiempo_maduracion prompt "Ingresa el tiempo de maduracion: ";
+    accept v_bodega char prompt "Ingresa el nombre de la bodega: ";
+    accept v_clasificacion char prompt "Ingresa la clasificacion: ";
+
+exec pr_insertar_marca('&v_nombre', '&v_elaboracion', '&v_cata_propia', '&v_maridaje', &v_temperatura, '&v_unidad_temperatura', &v_ventana_de_consumo, '&v_grado_de_alcohol', '&v_acidez_total', '&v_ph', '&v_color', '&v_contacto_madera', '&v_tipo_tapon', &v_tiempo_maduracion, '&v_bodega', '&v_clasificacion');
