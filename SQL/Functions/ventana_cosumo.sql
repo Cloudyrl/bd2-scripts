5701c003e8790a07f1576902a316d6fae93b062b
pedroale <pedro.alejandro.pt@gmail.com>
2018-12-16 13:57:56 -0400

    Ventana de Consumo Resulta

--- SQL/Functions/ventana_cosumo.sql
+++ SQL/Functions/ventana_cosumo.sql
@@ -1,4 +1,4 @@
 Create or Replace Function ventanaConsumo(ventana_consumo number) return number is
 begin
-    return(ventana_consumo/12);
+    return round(ventana_consumo/12);
 end ventanaConsumo;
